1f18714faea9f459f3ace4bbe4d393a7aeb81073
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-16 11:16:44 +0200

    URL

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const cors = require('cors');
 const app = express();app.use(cors());
-const PORT = 8000; //process.env.PORT || 
+const PORT = process.env.PORT || 8000;
 
 const URI = 'mongodb://test:123@3.87.77.55/Test?retryWrites=true&w=majority';//mongo URI
 
@@ -30,28 +30,50 @@ app.get('/files', (req, res) => {
     });
 });
 
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOMEPAGE')
+    console.log('Success on getting homepage /')
+});
+
+app.post('/login', verifyToken, (req, res) => {
     //Mock user
     const user = {
         id: 1,
         username: 'brad',
         email: 'brad@gmail.com'
-    }
-    
-    jwt.sign({user}, 'sectetkey', (err, token) => {
+    } 
+
+    jwt.sign({user}, 'sectetkey', {expiresIn: '1h'}, (err, token) => {
         res.json({
             token
         });
     });
-    res.json({
-        message: 'Login Page'
+});
+
+app.post('posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
     });
 });
 
-app.get('/', (req, res) => {
-    res.send('THIS IS THE HOMEPAGE')
-    console.log('Success on getting homepage /')
-});
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT);
